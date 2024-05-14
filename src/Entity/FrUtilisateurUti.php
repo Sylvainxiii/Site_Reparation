a43f918b58848b56d6c7451caad39f3fd65403ff
Sylvainxiii <sylvainlacroix@protonmail.com>
2024-05-14 14:19:59 +0200

    Ajout champs dans entité utilsateur et recréation de l'authentification

--- src/Entity/FrUtilisateurUti.php
+++ src/Entity/FrUtilisateurUti.php
@@ -3,6 +3,7 @@
 namespace App\Entity;
 
 use App\Repository\FrUtilisateurUtiRepository;
+use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
@@ -26,6 +27,24 @@ class FrUtilisateurUti implements UserInterface, PasswordAuthenticatedUserInterf
      */
     #[ORM\Column]
     private ?string $password = null;
+
+    #[ORM\Column(length: 100, nullable: true)]
+    private ?string $uti_nom = null;
+
+    #[ORM\Column(length: 100, nullable: true)]
+    private ?string $uti_prenom = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE, nullable: true)]
+    private ?\DateTimeInterface $uti_naissance_date = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $uti_avatar = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE, nullable: true)]
+    private ?\DateTimeInterface $uti_date_add = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE, nullable: true)]
+    private ?\DateTimeInterface $uti_date_edit = null;
 
     public function getId(): ?int
     {
@@ -95,5 +114,77 @@ class FrUtilisateurUti implements UserInterface, PasswordAuthenticatedUserInterf
     {
         // If you store any temporary, sensitive data on the user, clear it here
         // $this->plainPassword = null;
+    }
+
+    public function getUtiNom(): ?string
+    {
+        return $this->uti_nom;
+    }
+
+    public function setUtiNom(?string $uti_nom): static
+    {
+        $this->uti_nom = $uti_nom;
+
+        return $this;
+    }
+
+    public function getUtiPrenom(): ?string
+    {
+        return $this->uti_prenom;
+    }
+
+    public function setUtiPrenom(?string $uti_prenom): static
+    {
+        $this->uti_prenom = $uti_prenom;
+
+        return $this;
+    }
+
+    public function getUtiNaissanceDate(): ?\DateTimeInterface
+    {
+        return $this->uti_naissance_date;
+    }
+
+    public function setUtiNaissanceDate(?\DateTimeInterface $uti_naissance_date): static
+    {
+        $this->uti_naissance_date = $uti_naissance_date;
+
+        return $this;
+    }
+
+    public function getUtiAvatar(): ?string
+    {
+        return $this->uti_avatar;
+    }
+
+    public function setUtiAvatar(?string $uti_avatar): static
+    {
+        $this->uti_avatar = $uti_avatar;
+
+        return $this;
+    }
+
+    public function getUtiDateAdd(): ?\DateTimeInterface
+    {
+        return $this->uti_date_add;
+    }
+
+    public function setUtiDateAdd(?\DateTimeInterface $uti_date_add): static
+    {
+        $this->uti_date_add = $uti_date_add;
+
+        return $this;
+    }
+
+    public function getUtiDateEdit(): ?\DateTimeInterface
+    {
+        return $this->uti_date_edit;
+    }
+
+    public function setUtiDateEdit(?\DateTimeInterface $uti_date_edit): static
+    {
+        $this->uti_date_edit = $uti_date_edit;
+
+        return $this;
     }
 }
