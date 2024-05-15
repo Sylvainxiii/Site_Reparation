d65530971e45598578b5a13c235a625d1bd78437
Sylvainxiii <sylvainlacroix@protonmail.com>
2024-05-15 15:25:05 +0200

    Correction erreur utilisateur choix role

--- src/Form/FrUtilisateurUtiType.php
+++ src/Form/FrUtilisateurUtiType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\FrUtilisateurUti;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,7 +14,16 @@ class FrUtilisateurUtiType extends AbstractType
     {
         $builder
             ->add('email')
-            ->add('roles')
+            ->add('roles',ChoiceType::class, [
+                'choices' => [
+                    'Utilisateur' => 'ROLE_USER',
+                    // 'Employé' => 'ROLE_EMPLOYEE',
+                    'Admin' => 'ROLE_ADMIN'
+                ],
+                'help' => 'Sélectionner un rôle',
+                'multiple' => false,
+                'mapped' => false,
+            ])
             ->add('password')
             ->add('uti_nom')
             ->add('uti_prenom')
