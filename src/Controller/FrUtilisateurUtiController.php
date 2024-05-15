9d219bdbfad84572e876daa71086df3396e34eed
Sylvainxiii <sylvainlacroix@protonmail.com>
2024-05-15 22:40:37 +0200

    Abandon de Bulma au profit de bootsrap

--- src/Controller/FrUtilisateurUtiController.php
+++ src/Controller/FrUtilisateurUtiController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/fr/utilisateur/uti')]
+#[Route('/utilisateur')]
 class FrUtilisateurUtiController extends AbstractController
 {
     #[Route('/', name: 'app_fr_utilisateur_uti_index', methods: ['GET'])]
