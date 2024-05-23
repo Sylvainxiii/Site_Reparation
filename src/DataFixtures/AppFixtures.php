83261031d7545a42d42e6e45dab078ecbdc91cd7
Sylvainxiii <sylvainlacroix@protonmail.com>
2024-05-23 11:00:36 +0200

    Ajout d'un premier utilisateur avec fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -5,15 +5,26 @@ namespace App\DataFixtures;
 use App\Entity\FrUtilisateurUti;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
+    protected $encoder;
+
+    public function __construct(UserPasswordHasherInterface $encoder)
+    {
+        $this->encoder = $encoder;
+    }
+
     public function load(ObjectManager $manager): void
     {
         $user = new FrUtilisateurUti();
         $user->setUtiNom('LACROIX');
         $user->setUtiPrenom('Sylvain');
         $user->setEmail('sylvainlacroix@protonmail.com');
+        $encoded = $this->encoder->hashPassword($user, 'Trivisy81330');
+        $user->setPassword($encoded);
+        $user->setRoles(['ROLE_ADMIN']);
 
         $manager->persist($user);
 
